867937ccddab1b06137b0a06ef0e0411ee0c5e15
YoungChin <kingtide01@gmail.com>
2018-08-22 17:04:59 +0800

    1st

---
+++ BeatDemo/BeatDemo/Class/Main/Business/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  BeatDemo
+//
+//  Created by X Young. on 2018/8/22.
+//  Copyright © 2018年 X Young. All rights reserved.
+//
+
+import UIKit
+import AudioKitUI
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
