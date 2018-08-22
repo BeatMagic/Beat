b087f89840980c40c1117a04bf1ddd04f96648b8
YoungChin <kingtide01@gmail.com>
2018-08-22 17:24:59 +0800

    基础区域划分

--- BeatDemo/BeatDemo/Class/Main/Business/ViewController.swift
+++ BeatDemo/BeatDemo/Class/Main/Business/ViewController.swift
@@ -7,20 +7,29 @@
 //
 
 import UIKit
+import ChameleonFramework
 import AudioKitUI
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+}
 
-
+extension ViewController {
+    
+    /// 设置UI
+    func setUI() -> Void {
+//        self.view.backgroundColor = UIColor.flatWhite
+    }// funcEnd
 }
 
